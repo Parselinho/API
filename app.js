2c2e755480844c6f0349b0b2bde80b81aff21d97
Parselinho <parselinho@gmail.com>
2023-03-13 22:39:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+/* Variable Declaration : */
 const divSearch = document.querySelector('.search-container');
 const divGallery = document.querySelector('#gallery');
 const body = document.querySelector('body');
 const url = 'https://randomuser.me/api/?results=12' // will take 12 random users everytime the page reload
+let globalData = [];
+
+/* Functions Declaration: */
 
 /* creating  the form and the input*/
 function createFormInput() {
@@ -96,10 +100,46 @@ function createModal(data) {
 
     divButton.insertAdjacentHTML('beforeend', html);
     divExceed.insertAdjacentHTML('beforeend', buttonsExceed);
+
+    //pressing the X - will close the modal
+    const strong = document.querySelector('strong');
+    strong.addEventListener('click', () => {
+        body.removeChild(divContainer);
+    })
+
+    //creating three variables that are arrays of elements.
+    const divModalChildren = Array.from(divModal.children);
+    const divButtonChildren = Array.from(divButton.children);
+    const buttonsExceedChildren = Array.from(divExceed.children);
+
+    // closing the modal if I press outside the modal
+    divContainer.addEventListener('click', (e) => {
+        // check if the clicked element is not a child of those elements and then remove the modal, the last 3 elements are converted from array to seperate elements
+        if(![divExceed , ...divModalChildren, ...divButtonChildren, ...buttonsExceedChildren].includes(e.target)) {
+            body.removeChild(divContainer);
+        }
+    })
+    
+    
 }
 
-
-let globalData = [];
+function eventModal(e) {
+    // Get an array of all the modal cards
+    const modalCards = globalData.map(result => result);
+    // Add a click event listener to the gallery container
+    divGallery.addEventListener('click', (e) => {
+        // Find the closest ancestor of the clicked element with the class "card"
+        const card = e.target.closest('.card');
+        // If the closest ancestor is a "card" and its parent is the gallery container
+        if (card && card.parentNode === divGallery) {
+            // Get the index of the card in the gallery
+            const index = Array.from(divGallery.children).indexOf(card); // source : https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/from
+            // Create a modal
+            createModal(modalCards[index]);
+        }
+    });
+}
+    /* fetch API Declartation : */
 
 //  send a single request to the API
 fetch(url, {
@@ -108,19 +148,13 @@ fetch(url, {
     }
 })
     .then(res => res.json())
+    // When the JSON data is received, create cards for each result and add an event listener to the gallery container
     .then(data => {
+        // Map the "results" array to an array of card elements using the "createCard" function
         const cards = data.results.map(createCard) 
         globalData = data.results;
+        // Add an event listener to the gallery container for opening modals
         eventModal();
     })
 
-    function eventModal(e) {
-        const modalCards = globalData.map(result => result);
-        divGallery.addEventListener('click', (e) => {
-            const card = e.target.closest('.card');
-            if (card && card.parentNode === divGallery) {
-                const index = Array.from(divGallery.children).indexOf(card);
-                createModal(modalCards[index]);
-            }
-        });
-    }
+
